7e05b249c651c207134b84d06e6ba40b110f7296
DonatasNoreika <donoras@gmail.com>
2025-10-07 18:46:22 +0300

    Pridėti šablonai

--- mysite/contacts/views.py
+++ mysite/contacts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Contact
 
-# Create your views here.
+def contacts(request):
+    context = {
+        'contacts': Contact.objects.all().order_by('-pk')
+    }
+    return render(request, template_name="contacts.html", context=context)
