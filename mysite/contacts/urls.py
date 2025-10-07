3e105e3cc15d97ca47122ad51e390332eb2afbd5
DonatasNoreika <donoras@gmail.com>
2025-10-07 18:59:40 +0300

    Pridėta paieška

--- mysite/contacts/urls.py
+++ mysite/contacts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.contacts, name="contacts"),
+    path('search/', views.search, name="search"),
 ]
--- mysite/contacts/views.py
+++ mysite/contacts/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from .models import Contact
+from django.db.models import Q
 
 def contacts(request):
     context = {
         'contacts': Contact.objects.all().order_by('-pk')
     }
     return render(request, template_name="contacts.html", context=context)
+
+
+def search(request):
+    query = request.GET.get("query", '')
+    context = {
+        'contacts': Contact.objects.filter(Q(name__icontains=query) | Q(email__icontains=query))
+    }
+    return render(request, template_name="partials/contact_list.html", context=context)
