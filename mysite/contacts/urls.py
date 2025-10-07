eea351d71f47cdbd6a44ec9782fca30eba1ab167
DonatasNoreika <donoras@gmail.com>
2025-10-07 19:27:14 +0300

    Pridėtas trynimas

--- mysite/contacts/urls.py
+++ mysite/contacts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.contacts, name="contacts"),
     path('search/', views.search, name="search"),
+    path('contacts/<int:contact_id>/delete', views.delete_contact, name="delete_contact"),
 ]
--- mysite/contacts/views.py
+++ mysite/contacts/views.py
@@ -15,3 +15,11 @@ def search(request):
         'contacts': Contact.objects.filter(Q(name__icontains=query) | Q(email__icontains=query))
     }
     return render(request, template_name="partials/contact_list.html", context=context)
+
+def delete_contact(request, contact_id):
+    contact = Contact.objects.get(pk=contact_id)
+    contact.delete()
+    context = {
+        'contacts': Contact.objects.all().order_by('-pk')
+    }
+    return render(request, template_name="partials/contact_list.html", context=context)
